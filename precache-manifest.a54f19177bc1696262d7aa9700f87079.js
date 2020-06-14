e0b4dad1f3faed2ceec26e9f4ab6f8420d872cfa
Rohit Thorat <thorat.rohit95@gmail.com>
2020-06-14 12:50:38 +0530

    Updates

---
+++ precache-manifest.a54f19177bc1696262d7aa9700f87079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d9e01e17654ffb2756f9832c6f9e98",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "5142ab8d0be7164be258",
+    "url": "/monsters-rolodex-react/static/css/main.e428f68e.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex-react/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5142ab8d0be7164be258",
+    "url": "/monsters-rolodex-react/static/js/main.8141e8ae.chunk.js"
+  },
+  {
+    "revision": "4984d3118c6c771f0983",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.42c5dd98.js"
+  }
+]);
